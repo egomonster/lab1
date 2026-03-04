51a30422a67e8cd74154b2c4f137023b9974ceb8
egor <egazam@mail.ru>
2026-03-04 13:05:09 +0700

    write code

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]){
-
+    int i=0, k=0;
+    while(s1[i]){
+        int j=0;
+        int found=0;
+        while(s2[j] && !found){
+            if(s1[i]==s2[j]){
+                found=1;
+            }
+            j++;
+        }
+        if(!found){
+            s1[k++]=s1[i];
+        }
+        i++;
+    }
+    s1[k]='\0';
 }
 
 int main()
