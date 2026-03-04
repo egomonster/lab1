a54409f2907bd3dd8eac7eb1b69229d1a540854a
egor <egazam@mail.ru>
2026-03-04 13:04:21 +0700

    create new function

--- main.c
+++ main.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
+void squeeze(char s1[], char s2[]){
+
+}
+
 int main()
 {
     char s1[]="asdfghjkl";
-    char s2[]="asdfe";
+    char s2[]="sdafe";
 
-    int i=0, k=0;
-    while(s1[i]!="\0"){
-        int j=0;
-        int found=0;
-        while(s2[j]!="\0"){
-            if(s1[i]==s2[j]){
-                found=1;
-            }
-            j++;
-        }
-        if(!found){
-            s1[k++]=s1[i];
-        }
-        i++;
-    }
-    s1[k]="\0";
-
-
+    squeeze(s1,s2);
     printf("%s\n",s1);
+
     return 0;
 }
