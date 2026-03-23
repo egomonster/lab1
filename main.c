2ef93c59929c46438996d5a511347e0d2f290e8f
egor <egazam@mail.ru>
2026-03-23 17:15:08 +0700

    write code and create function

--- main.c
+++ main.c
@@ -17,28 +17,88 @@ void squeeze(char s1[], char s2[]){
         i++;
     }
     s1[k]='\0';
+    printf("%s\n",s1);
 }
 
-void isDecMult(char text){
-
+int charToValue(char text){
+    if(text>='0'&&text<='9'){
+        return text-'0';
+    }
+    return text-'A'+10;
 }
 
-void printDifWords(char sq, char W){
+void isDecMult4(char text[], int base){
+    int len=0;
+    while(text[len]!='\0'){
+        len++;
+    }
+    int value;
+    if(len==1){
+        value=charToValue(text[0]);
+    }
+    else{
+        int a=charToValue(text[len-2]);
+        int b=charToValue(text[len-1]);
+        value=a*base+b;
+    }
+    if(value%4==0){
+        printf("yes\n");
+    }
+    else{
+        printf("no\n");
+    }
+}
 
+void printDifWords(char *sq, char *W){
+    int i=0;
+    int wlen=0;
+    char tmp[100];
+    while(W[wlen]){
+        wlen++;
+    }
+    while(sq[i]){
+        while(sq[i]&&(sq[i]==','||sq[i]==' '||sq[i]=='.')){
+            i++;
+        }
+        if(!sq[i]){
+            break;
+        }
+        int j=0;
+        while(sq[i]&&sq[i]!=','&&sq[i]!=' '&&sq[i]!='.'){
+            tmp[j++]=sq[i++];
+        }
+        tmp[j]='\0';
+        int sqlen=j;
+        int eq=(sqlen==wlen);
+        if(eq){
+            int k=0;
+            while(k<sqlen){
+                if(tmp[k]!=W[k]){
+                    eq=0;
+                    break;
+                }
+                k++;
+            }
+        }
+        if(!eq){
+            printf("%s ",tmp);
+        }
+    }
 }
 
 int main()
 {
-    char s1[]="asdfghjkl";
+    char s1[]="asndfghasjklar";
     char s2[]="sdafe";
-
     squeeze(s1,s2);
-    printf("%s\n",s1);
 
-    char text[]="1234";
+    char text[]="1A";
+    int base=16;
+    isDecMult4(text,base);
 
-    char sq[]="aaa, bbb, ccc.";
+    char sq[]="aaa, bbb, bbbc, bbbb, bb.";
     char W[]="bbb";
+    printDifWords(sq,W);
 
     return 0;
 }
